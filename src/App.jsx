15088cd337eed11ad9e5c7b6debad2ddec2cc360
tahanashad <tahanaushad6@gmail.com>
2024-04-25 21:50:03 +0500

    -Filtered feature added

--- src/App.jsx
+++ src/App.jsx
@@ -1,70 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
-import { useState } from 'react';
-import BookShelf from './BookShelf';
-
-
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+import BookShelf from "./BookShelf";
 
 function App() {
-
-  const [books, setBooks] = useState([])
-  const [filtered, setFiltered] = useState([])
+  const [books, setBooks] = useState([]);
+  const [filtered, setFiltered] = useState([]);
+  const [alpfilter, setAlpfilter] = useState(null);
 
   const addBook = (bookObj) => {
     setBooks((previousState) => {
-      return [
-        ...previousState,
-        bookObj
-      ]
-    })
-  }
+      return [...previousState, bookObj];
+    });
+  };
 
-
-  const removeBook = (index) => {
-    let booksObj = books;
-    booksObj.splice(index, 1);
-    setBooks([...booksObj])
-  }
+  const removeBook = (index, id) => {
+    books.splice(index, 1);
+    setBooks([...books]);
+    if (filtered.length > 0) {
+      setFiltered((prev) => {
+        return prev.filter((d) => d.id !== id);
+      });
+    }
+  };
 
   const onSearch = (event) => {
     const filteredBook = books.filter((book, index) => {
       return book.name.includes(event.target.value);
-    })
-    setFiltered([...filteredBook])
-  }
+    });
+    setFiltered([...filteredBook]);
+  };
 
+  const onSelectAlpfilter = (selectedAlp) => {
+    if (selectedAlp === "All") {
+      setAlpfilter(null);
+      setFiltered([])
+      return;
+    }
+    setAlpfilter(selectedAlp);
+    let filteredByAlp = books.filter((book, index) => {
+      return book.name.charAt(0).toLowerCase() === selectedAlp.toLowerCase();
+    });
+    console.log(filteredByAlp);
+    setFiltered(filteredByAlp);
+  };
 
   return (
-    <div className='main'>
-
-      <div className='heading'>
+    <div className="main">
+      <div className="heading">
         <h1>Book Shelf</h1>
         <h4>{books.length} Books Available</h4>
       </div>
 
       <div className="wrap">
         <div className="search">
-          <input onChange={onSearch} type="text" className="searchTerm" placeholder="Search Book.." />
+          <input
+            onChange={onSearch}
+            type="text"
+            className="searchTerm"
+            placeholder="Search Book.."
+          />
           <button type="submit" className="searchButton">
             <i className="fas fa-search"></i>
           </button>
         </div>
       </div>
 
-
-
-      <BookShelf addBook={addBook} books={books} filtered={filtered} removeBook={removeBook} />
-
-
+      <BookShelf
+        addBook={addBook}
+        books={books}
+        filtered={filtered}
+        removeBook={removeBook}
+        alpfilter={alpfilter}
+        onSelectAlpfilter={onSelectAlpfilter}
+      />
     </div>
   );
-
-
-
-
-};
-
-
-
+}
 
 export default App;
--- src/BookShelf.jsx
+++ src/BookShelf.jsx
@@ -1,89 +1,135 @@
+import { render } from "@testing-library/react";
 import { useState } from "react";
 
+let alphabets = [
+  "All",
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+
 const BookShelf = (props) => {
-    const [showForm, setShowForm] = useState(false)
+  const [showForm, setShowForm] = useState(false);
 
-    const switchAddForm = () => {
-        setShowForm(!showForm)
-    }
+  const switchAddForm = () => {
+    setShowForm(!showForm);
+  };
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const obj = {
+        id: new Date ().getTime(),
+      name: event.target.bookName.value,
+      author: event.target.author.value,
+      publisher: event.target.publisher.value,
+      description: event.target.bookDescription.value,
+    };
+    props.addBook(obj);
 
+    event.target.bookName.value = "";
+    event.target.author.value = "";
+    event.target.publisher.value = "";
+    event.target.bookDescription.value = "";
+  };
 
-
-
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        const obj = {
-            name: event.target.bookName.value,
-            author: event.target.author.value,
-            publisher: event.target.publisher.value,
-            description: event.target.bookDescription.value
-        }
-        props.addBook(obj)
-
-
-        event.target.bookName.value = ""
-        event.target.author.value = ""
-        event.target.publisher.value = ""
-        event.target.bookDescription.value = ""
-
-
-
-    }
-
-    const renderList = (book, index) => {
-        return (
-            <div key={index} className="bookCard">
-                <img src="https://dictionary.cambridge.org/images/thumb/book_noun_001_01679.jpg?version=5.0.389" alt="Book Image" />
-                <p className="name"> {book.name} </p>
-                <h4> Author: {book.author} </h4>
-                <h5> Publisher: {book.publisher}</h5>
-                <h5>{book.description}</h5>
-                <button onClick={() => props.removeBook(index)}>Remove Book</button>
-            </div>
-        )
-    }
-console.log(props.filtered)
+  const renderList = (book, index) => {
     return (
-        <div>
-            <button onClick={switchAddForm} className="addbook">Add Book</button>
+      <div key={index} className="bookCard">
+        <img
+          src="https://dictionary.cambridge.org/images/thumb/book_noun_001_01679.jpg?version=5.0.389"
+          alt="Book Image"
+        />
+        <p className="name"> {book.name} </p>
+        <h4> Author: {book.author} </h4>
+        <h5> Publisher: {book.publisher}</h5>
+        <h5>{book.description}</h5>
+        <button onClick={() => props.removeBook(index, book.id)}>Remove Book</button>
+      </div>
+    );
+  };
 
-            {showForm && <form onSubmit={handleSubmit}>
-                <div className="addBox">
-                    <div className="myInputs">
-                        <label htmlFor="label">Book Name</label>
-                        <input type="text" className="inputF" name="bookName" />
-                    </div>
-                    <div className="myInputs">
-                        <label htmlFor="label">Author</label>
-                        <input type="text" className="inputF" name="author" />
-                    </div>
-                    <div className="myInputs">
-                        <label htmlFor="label">Publisher</label>
-                        <input type="text" className="inputF" name="publisher" />
-                    </div>
-                    <div className="myInputs">
-                        <label htmlFor="label">Description</label>
-                        <input type="text" className="inputF" name="bookDescription" />
-                    </div>
-                    <div>
-                        <button className="submitBtn" type="submit">Submit</button>
-                    </div>
-                </div>
-            </form>}
-            {props.filtered.length > 0 ? props.filtered.map((book, index) => {
-                return (
-                    renderList(book, index)
-                )
-            }) :
-                props.books.map((book, index) => {
-                    return (
-                        renderList(book, index)
-                    )
-                })}
-        </div>
-    )
+  return (
+    <div>
+      <button onClick={switchAddForm} className="addbook">
+        Add Book
+      </button>
 
-}
-
+      {showForm && (
+        <form onSubmit={handleSubmit}>
+          <div className="addBox">
+            <div className="myInputs">
+              <label htmlFor="label">Book Name</label>
+              <input type="text" className="inputF" name="bookName" />
+            </div>
+            <div className="myInputs">
+              <label htmlFor="label">Author</label>
+              <input type="text" className="inputF" name="author" />
+            </div>
+            <div className="myInputs">
+              <label htmlFor="label">Publisher</label>
+              <input type="text" className="inputF" name="publisher" />
+            </div>
+            <div className="myInputs">
+              <label htmlFor="label">Description</label>
+              <input type="text" className="inputF" name="bookDescription" />
+            </div>
+            <div>
+              <button className="submitBtn" type="submit">
+                Submit
+              </button>
+            </div>
+          </div>
+        </form>
+      )}
+      {alphabets.map((alphabet, index) => {
+        return (
+          <span
+            key={alphabet}
+            onClick={() => props.onSelectAlpfilter(alphabet)}
+            className={`alphabet ${alphabet === props.alpfilter ? "selected" : ""}`}
+          >
+            {alphabet}
+          </span>
+        );
+      })}
+      {props.filtered.length > 0 || props.alpfilter
+        ? props.filtered.map((book, index) => {
+            return renderList(book, index);
+          })
+        : props.books.map((book, index) => {
+            return renderList(book, index);
+          })}
+    </div>
+  );
+};
+BookShelf.defaultProps = {
+  addBook: () => {},
+  books: [],
+  filtered: [],
+  removeBook: () => {},
+  alpfilter: null,
+};
 export default BookShelf;
